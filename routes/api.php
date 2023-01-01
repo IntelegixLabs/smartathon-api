6b060c3f0068553a5efe4e1383113348496eed48
sayan <sayansinha5@gmail.com>
2023-01-01 11:15:44 +0530

    :truck: add new routes for login and register

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Misc\CoOrdinateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login', 'login');
+    Route::post('/register', 'register');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Test Route Working
+|--------------------------------------------------------------------------
+|
+| Test if API works for getting users
 |
 */
 
