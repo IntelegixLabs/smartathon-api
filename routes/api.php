e7f203ab428136fb03b1d5b19fd3b93626460285
sayan <sayansinha5@gmail.com>
2023-01-05 21:22:38 +0530

    :truck: :refactor: add route logout and refactor register, login in api

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 /*
 |--------------------------------------------------------------------------
