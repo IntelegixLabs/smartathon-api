1b3c666b7022d5786abe80f5cda8371e7be84bd2
sayan <sayansinha5@gmail.com>
2023-01-07 23:42:59 +0530

    :card_file_box: auto timestamp in users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -36,7 +36,9 @@ return new class extends Migration
 
             $table->rememberToken();
 
-            $table->timestamps();
+            $table->timestamp('created_at')->useCurrent();
+
+            $table->timestamp('updated_at')->useCurrent();
         });
     }
 
