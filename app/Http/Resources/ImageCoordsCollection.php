462646b5b053907cc5db8c8dff7f9275650594b1
sayan <sayansinha5@gmail.com>
2023-01-18 01:13:13 +0530

    :bug: fix result required from ImageCoordsCollection

--- app/Http/Resources/ImageCoordsCollection.php
+++ app/Http/Resources/ImageCoordsCollection.php
@@ -14,7 +14,24 @@ class ImageCoordsCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        // return parent::toArray($request);
-        return $this->collection;
+        return $this->collection->transform(function ($pollution) {
+            return [
+                'id' => $pollution->id,
+                'user_name' => $pollution->user->full_name,
+                'latitude' => $pollution->latitude,
+                'longitude' => $pollution->longitude,
+                'unfixed_image' => $pollution->unfixed_image,
+                'x' => $pollution->x,
+                'y' => $pollution->y,
+                'w' => $pollution->w,
+                'z' => $pollution->z,
+                'pollution_id' => $pollution->pollution_id,
+                'type' => $pollution->pollution->type,
+                'is_fixed' => $pollution->is_fixed,
+                'fixed_image' => $pollution->fixed_image,
+                'created_at' => $pollution->created_at,
+                'updated_at' => $pollution->updated_at
+            ];
+        });
     }
 }
