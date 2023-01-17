e12b103b12452ce6209b4b47cbfac25f5f055963
sayan <sayansinha5@gmail.com>
2023-01-18 01:16:22 +0530

    :seedling: :building_construction: add 'image_name' seed to PollutionSeeder

--- database/seeders/PollutionSeeder.php
+++ database/seeders/PollutionSeeder.php
@@ -15,17 +15,17 @@ class PollutionSeeder extends Seeder
     public function run()
     {
         Pollution::insert([
-            ['kind' => 'visual', 'type' => 'GRAFFITI'],
-            ['kind' => 'visual', 'type' => 'FADED SIGNAGE'],
-            ['kind' => 'visual', 'type' => 'POTHOLES'],
-            ['kind' => 'visual', 'type' => 'GARBAGE'],
-            ['kind' => 'visual', 'type' => 'CONSTRUCTION ROAD'],
-            ['kind' => 'visual', 'type' => 'BROKEN SIGNAGE'],
-            ['kind' => 'visual', 'type' => 'BAD STREETLIGHT'],
-            ['kind' => 'visual', 'type' => 'BAD BILLBOARD'],
-            ['kind' => 'visual', 'type' => 'SAND ON ROCK'],
-            ['kind' => 'visual', 'type' => 'CLUTTER SIDEWALK'],
-            ['kind' => 'visual', 'type' => 'UNKEPT FACADE'],
+            ['kind' => 'visual', 'type' => 'GRAFFITTI', 'image_name' => 'img_graffitti.png'],
+            ['kind' => 'visual', 'type' => 'FADED SIGNAGE', 'image_name' => 'img_faded_signage.png'],
+            ['kind' => 'visual', 'type' => 'POTHOLES', 'image_name' => 'img_potholes.png'],
+            ['kind' => 'visual', 'type' => 'GARBAGE', 'image_name' => 'img_garbage.png'],
+            ['kind' => 'visual', 'type' => 'CONSTRUCTION ROAD', 'image_name' => 'img_construction_road.png'],
+            ['kind' => 'visual', 'type' => 'BROKEN SIGNAGE', 'image_name' => 'img_broken_signage.png'],
+            ['kind' => 'visual', 'type' => 'BAD STREETLIGHT', 'image_name' => 'img_bad_streetlight.png'],
+            ['kind' => 'visual', 'type' => 'BAD BILLBOARD', 'image_name' => 'img_bad_billboard.png'],
+            ['kind' => 'visual', 'type' => 'SAND ON ROCK', 'image_name' => 'img_sand_on_rock.png'],
+            ['kind' => 'visual', 'type' => 'CLUTTER SIDEWALK', 'image_name' => 'img_clutter_sidewalk.png'],
+            ['kind' => 'visual', 'type' => 'UNKEPT FACADE', 'image_name' => 'img_unkept_facade.png'],
         ]);
     }
 }
